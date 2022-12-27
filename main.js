ce714abba58dc3a08fe12979d471ab83e1fa0070
Hillel Wasserman <hillelwasser95@gmail.com>
2022-12-27 11:34:53 +0200

    update27.12.22

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById('darkButton');
-btn.addEventListener('mouseover',function(){
-})
+// btn.addEventListener('mouseover',function(){
+// })
 
 
 let isDarkModeEnabled = false; 
@@ -15,3 +15,14 @@ else {
     document.body.style.backgroundImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_x4a_34dyEfU9rvDWwD4FLrDEpfzdM2FxuIZrFx0QwA&s');
 }
 }
+
+const favoriteFood = ['pizza','Cheese','Tomato','apples','honey'];
+let makelist = '<ul id="food-list">';
+for (let index = 0;index < favoriteFood.length;index++) {
+    let element = favoriteFood[index];
+    makelist+= `<li>${element}</li>`;
+}
+makelist +='</ul>';
+console.log(makelist);
+document.getElementById("food-list").outerHTML = makelist;
+
