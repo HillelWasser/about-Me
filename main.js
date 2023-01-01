d9b64a629a43d9bc04179c16c9a403dccb587ba5
Hillel Wasserman <hillelwasser95@gmail.com>
2023-01-01 18:16:42 +0200

    1.1.23

--- main.js
+++ main.js
@@ -26,3 +26,104 @@ makelist +='</ul>';
 console.log(makelist);
 document.getElementById("food-list").outerHTML = makelist;
 
+// const imagesDataBase = [
+// img1 = {
+//     url:"/assets/dog1.jpeg",
+//     description: 'the dog i miss the most',
+//     alt: 'dog 1'
+// },
+// img2 = {
+//     url:"/assets/dog2.jpeg" ,
+//     description: 'my family dog',
+//     alt: 'dog 2'
+// },
+// img3 = {
+//     url:"/assets/dog3.jpeg" ,
+//     description: 'my favorite dog',
+//     alt: 'dog 3'
+// },
+// ]
+
+// function createGallery(imagesDataBase) {
+//     // create an empty div element
+//     let gallery = document.createElement('div');
+//     // set the class of the div to 'gallery'
+//     gallery.classList.add('gallery');
+  
+//     // loop through the imagesDataBase array
+//     for (let i = 0; i < imagesDataBase.length; i++) {
+//       // create an img element
+//       let img = document.createElement('img');
+//       // set the src and alt attributes of the img element
+//       img.src = imagesDataBase[i].src;
+//       img.alt = imagesDataBase[i].alt;
+      
+//       // create a p element
+//       let desc = document.createElement('p');
+//       // set the text content of the p element to the description of the item
+//       desc.textContent = imagesDataBase[i].description;
+  
+//       // append the img and p elements to the gallery div
+//       gallery.appendChild(img);
+//       gallery.appendChild(desc);
+//     }
+  
+//     // return the gallery div
+//      gallery.innerHTML;
+//   }
+
+//   createGallery()
+const imagesDataBase = [
+    {
+      src: "/assets/dog1.jpeg",
+      description: 'the dog i miss the most',
+      alt: 'dog 1'
+    },
+    {
+      src: "/assets/dog2.jpeg",
+      description: 'my family dog',
+      alt: 'dog 2'
+    },
+    {
+      src: "/assets/dog3.jpeg",
+      description: 'my favorite dog',
+      alt: 'dog 3'
+    },
+  ];
+  
+  function createGallery(imagesDataBase) {
+    // create an empty div element
+    let gallery = document.createElement('div');
+    // set the class of the div to 'gallery'
+    gallery.classList.add('gallery');
+  
+    // loop through the imagesDataBase array
+    for (let i = 0; i < imagesDataBase.length; i++) {
+      // create an img element
+      let img = document.createElement('img');
+      img.classList.add('images');
+      // set the src and alt attributes of the img element
+      img.src = imagesDataBase[i].src;
+      img.alt = imagesDataBase[i].alt;
+  
+      // create a p element
+      let desc = document.createElement('p');
+      // set the text content of the p element to the description of the item
+      desc.textContent = imagesDataBase[i].description;
+      desc.classList.add('descImg');
+  
+      // append the img and p elements to the gallery div
+      gallery.appendChild(img);
+      gallery.appendChild(desc);
+    }
+  
+    // append the gallery element to the DOM
+    document.body.appendChild(gallery);
+  
+    // return the gallery element
+    return gallery;
+  }
+  
+  createGallery(imagesDataBase);
+
+
